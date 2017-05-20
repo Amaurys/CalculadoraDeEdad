2b44f9874a7a515e8242a75fc6240139d904a003
GogoMix <sanchezamaurys@gmail.com>
2017-05-20 07:49:35 -0400

    DatePicker and others component were added. An action in the main activity to calculate the age of someone was added too. Some string values were added.

--- app/src/main/java/com/sanchezamaurys/calculadoradeedad/MainActivity.java
+++ app/src/main/java/com/sanchezamaurys/calculadoradeedad/MainActivity.java
@@ -2,12 +2,33 @@ package com.sanchezamaurys.calculadoradeedad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.StringBuilderPrinter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private DatePicker datePicker;
+    private Button btnCalular;
+    private TextView tvEdad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        datePicker = (DatePicker) findViewById(R.id.myDatePicker);
+        tvEdad = (TextView) findViewById(R.id.textViewEdadCalculada);
+
+        findViewById(R.id.btnCalcularEdad).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        StringBuilder sb = new StringBuilder();
+        int año = 2017 - datePicker.getYear();
+        tvEdad.setText("Usted tiene "+año +" años de edad.");
     }
 }
